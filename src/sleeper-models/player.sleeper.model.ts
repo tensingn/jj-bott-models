7df7113ccdbda10a24ae878d5eb64bf2f186e958
tensingn <tensingn@oregonstate.edu>
2024-04-30 07:31:39 -0400

    updating player sleeper model

--- src/sleeper-models/player.sleeper.model.ts
+++ src/sleeper-models/player.sleeper.model.ts
@@ -1,9 +1,12 @@
+import { PositionNames } from "../names/position.names";
+
 export class PlayerSleeperModel {
 	player_id: string;
 	status: string;
-	fantasy_positions: Array<string>;
+	fantasy_positions: Array<PositionNames>;
 	last_name: string;
 	first_name: string;
 	full_name: string;
 	team: string;
+	espn_id: string;
 }
