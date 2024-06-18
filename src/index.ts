63c699f90be90591117413996b2624ab0d7281bd
tensingn <tensingn@oregonstate.edu>
2024-06-17 20:48:28 -0400

    forgot to add nfl game model to index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { PlayerModel } from "./models/player.model.js";
 export { TeamModel } from "./models/team.model.js";
 export { PlayerGameModel } from "./models/player-game.model.js";
 export { NFLTeamModel } from "./models/nfl-team.model.js";
+export { NFLGameModel } from "./models/nfl-game.model.js";
 
 // names
 export { NFLTeamNames, NFLTeamNamesArray } from "./names/nfl-team.names.js";
